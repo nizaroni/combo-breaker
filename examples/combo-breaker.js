8309c1290098431c79dd5e1546a88554b4c5259c
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-06-21 20:26:22 -0400

    Add `keep` option for keeping more repetitions
    By sending an `options.keep` integer you can control how many instances of the combo are left over. Defaults to `1`.

--- examples/combo-breaker.js
+++ examples/combo-breaker.js
@@ -4,6 +4,9 @@ var comboBreaker = require('..'),
 ;
 
 redundant = 'I really really really really love pizza.';
-broken = comboBreaker(redundant, 'really ');
 
+broken = comboBreaker(redundant, 'really ');
 console.log(broken); // "I really love pizza."
+
+broken = comboBreaker(redundant, 'really ', { keep: 2 });
+console.log(broken); // "I really really love pizza."
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function comboBreaker (str, combo) {
-    var pattern,
-        regex
+function comboBreaker (str, combo, options) {
+    var leftOver,
+        pattern,
+        regex,
+        count,
+        keep
     ;
 
-    pattern = combo + '(' + combo + ')+';
+    options = options || {};
+    keep = options.keep || 1;
+
+    leftOver = '';
+    for (count = 0; count < keep; count += 1) {
+        leftOver += combo;
+    }
+
+    pattern = leftOver + '(' + combo + ')+';
     regex = new RegExp(pattern, 'g');
-    return str.replace(regex, combo);
+
+    return str.replace(regex, leftOver);
 }
 
 module.exports = comboBreaker;
