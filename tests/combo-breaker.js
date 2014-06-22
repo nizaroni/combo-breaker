559ebd8e365d272acf29c4107cf95e1fe20b3cfa
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2014-06-22 15:55:00 -0400

    Add failing tests for regex meta characters

--- tests/combo-breaker.js
+++ tests/combo-breaker.js
@@ -17,3 +17,47 @@ tape('`options.keep`', function (t) {
     t.equal(broken, 'I really really love pizza.');
     t.end();
 });
+
+tape('escaping of regex characters', function (t) {
+    t.test('├─ `$` character', function (t) {
+        broken = comboBreaker('a million dollar$$$$$$$$$$$$$$$', '$');
+        t.equal(broken, 'a million dollar$', '$ character');
+        t.end();
+    });
+
+    t.test('├─ `*` character', function (t) {
+        broken = comboBreaker('i can tell no lies***********', '*');
+        t.equal(broken, 'i can tell no lies*');
+        t.end();
+    });
+
+    t.test('├─ `)` character', function (t) {
+        broken = comboBreaker('())))))))))))))', ')');
+        t.equal(broken, '()');
+        t.end();
+    });
+
+    t.test('├─ `|` character', function (t) {
+        broken = comboBreaker('pipe pipe pipe ||||| pipe', '|');
+        t.equal(broken, 'pipe pipe pipe | pipe');
+        t.end();
+    });
+
+    t.test('├─ `.` character', function (t) {
+        broken = comboBreaker('dot dot dot ........ dot dot', '.');
+        t.equal(broken, 'dot dot dot . dot dot');
+        t.end();
+    });
+
+    t.test('├─ `?` character', function (t) {
+        broken = comboBreaker('why???????', '?');
+        t.equal(broken, 'why?');
+        t.end();
+    });
+
+    t.test('└─ `\\` character', function (t) {
+        broken = comboBreaker('back slash slash \\\\\\\\ slash', '\\');
+        t.equal(broken, 'back slash slash \\ slash');
+        t.end();
+    });
+});
